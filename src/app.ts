e685a912e7bcd459dfdd2b370a10bc53a2b009e1
JoaquinAO <joaquinaravena@protonmail.com>
2023-06-12 21:06:07 -0400

    security

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,31 @@ import express, { Application } from 'express'
 import routerHealth from './helpers/health'
 import HandlerErrors from './helpers/errors'
 import userRoutes from './modules/user/interfaces/http/user.routes'
+import helmet from 'helmet'
+import hpp from 'hpp'
+import cors from 'cors'
+import compression from 'compression'
 class App {
   readonly expressApp: Application
   constructor() {
     this.expressApp = express()
+    this.mountSecurity()
     this.mountHealthCheck()
     this.mountMiddlewares()
     this.mountRoutes()
     this.mountError()
   }
+  mountSecurity() {
+    this.expressApp.use(helmet())
+    this.expressApp.use(hpp())
+    this.expressApp.use(cors(
+      {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      }
+    ))
+  }
+
   mountHealthCheck() {
     this.expressApp.use('/', routerHealth)
   }
@@ -19,7 +35,7 @@ class App {
   }
   mountMiddlewares() {
     this.expressApp.use(express.json())
-    // TODO: Add middlewares
+    this.expressApp.use(compression())
   }
   mountRoutes() {
     this.expressApp.use('/user', userRoutes)
